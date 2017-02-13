05cd5e5f8da19a3fe652490a7d2ddc00488a44f4
Nenad <norlovic@gmail.com>
2017-02-13 20:28:39 +0100

    minimalni toolbar dodat

--- app/src/main/java/com/example/androiddevelopment/zadatakglumci/activities/ActivityFirst.java
+++ app/src/main/java/com/example/androiddevelopment/zadatakglumci/activities/ActivityFirst.java
@@ -3,10 +3,15 @@ package com.example.androiddevelopment.zadatakglumci.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.androiddevelopment.zadatakglumci.R;
 import com.example.androiddevelopment.zadatakglumci.providers.GlumacProvider;
@@ -17,12 +22,23 @@ import java.util.List;
  * Created by androiddevelopment on 13.2.17..
  */
 
-public class ActivityFirst extends Activity {
+public class ActivityFirst extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_first);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+         //   actionBar.setDisplayHomeAsUpEnabled(true);
+           // actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+            //actionBar.setHomeButtonEnabled(true);
+            actionBar.show();
+        }
 
 		final List<String> glumciImena = GlumacProvider.getGlumciImena();
 		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.list_item, glumciImena);
@@ -39,4 +55,31 @@ public class ActivityFirst extends Activity {
 		});
 
     }
+
+	// onOptionsIActivitytemSelected method is called whenever an item in the Toolbar is selected.
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_create:
+				//Toast.makeText(this, "Action " + getString(R.string.fragment_master_action_create) + " executed.", Toast.LENGTH_SHORT).show();
+				Toast.makeText(this, "Action Dodaj executed.", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.action_update:
+				//Toast.makeText(this, "Action " + getString(R.string.fragment_detal_action_update) + " executed.", Toast.LENGTH_SHORT).show();
+				Toast.makeText(this, "Action Izmeni executed.", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.action_delete:
+				//Toast.makeText(this, "Action " + getString(R.string.fragment_detal_action_delete) + " executed.", Toast.LENGTH_SHORT).show();
+				Toast.makeText(this, "Action Brisi executed.", Toast.LENGTH_SHORT).show();
+				break;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+	// onCreateOptionsMenu method initialize the contents of the Activity's Toolbar.
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.action_bar, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 }
--- app/src/main/java/com/example/androiddevelopment/zadatakglumci/activities/ActivitySecond.java
+++ app/src/main/java/com/example/androiddevelopment/zadatakglumci/activities/ActivitySecond.java
@@ -3,11 +3,16 @@ package com.example.androiddevelopment.zadatakglumci.activities;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.androiddevelopment.zadatakglumci.R;
 import com.example.androiddevelopment.zadatakglumci.model.Film;
@@ -23,7 +28,7 @@ import java.util.List;
  * Created by androiddevelopment on 13.2.17..
  */
 
-public class ActivitySecond extends Activity {
+public class ActivitySecond extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +38,16 @@ public class ActivitySecond extends Activity {
 
         final int position = getIntent().getIntExtra("position", 0);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            //   actionBar.setDisplayHomeAsUpEnabled(true);
+            // actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+            //actionBar.setHomeButtonEnabled(true);
+            actionBar.show();
+        }
 
         TextView tvImePrezime = (TextView) findViewById(R.id.tv_imeprezime);
         tvImePrezime.setText(GlumacProvider.getGlumacById(position).getImePrezime());
@@ -75,5 +90,32 @@ public class ActivitySecond extends Activity {
         ListView listView = (ListView) findViewById(R.id.listaFilmovi);
 
         listView.setAdapter(dataAdapter);
+    }
+
+    // onOptionsIActivitytemSelected method is called whenever an item in the Toolbar is selected.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create:
+                //Toast.makeText(this, "Action " + getString(R.string.fragment_master_action_create) + " executed.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Action Dodaj executed.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_update:
+                //Toast.makeText(this, "Action " + getString(R.string.fragment_detal_action_update) + " executed.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Action Izmeni executed.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_delete:
+                //Toast.makeText(this, "Action " + getString(R.string.fragment_detal_action_delete) + " executed.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Action Brisi executed.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    // onCreateOptionsMenu method initialize the contents of the Activity's Toolbar.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
